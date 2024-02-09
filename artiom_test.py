832b648afdfa81f974c77a5a87e270932ce2001e
artiom.matvei <artiommatvei@gmail.com>
2024-02-08 21:40:01 -0500

    initial commit

---
+++ artiom_test.py
@@ -0,0 +1,24 @@
+from word2vec import *
+
+
+if __name__ == "__main__":
+  
+  def test_naiveSoftmaxLossAndGradient():
+    """ Test naiveSoftmaxLossAndGradient """
+    dataset, dummy_vectors, dummy_tokens = getDummyObjects()
+
+    print("==== Gradient check for naiveSoftmaxLossAndGradient ====")
+    def temp(vec):
+        loss, gradCenterVec, gradOutsideVecs = naiveSoftmaxLossAndGradient(vec, 1, dummy_vectors, dataset)
+        return loss, gradCenterVec
+    gradcheck_naive(temp, np.random.randn(3), "naiveSoftmaxLossAndGradient gradCenterVec")
+
+    centerVec = np.random.randn(3)
+    def temp(vec):
+        loss, gradCenterVec, gradOutsideVecs = naiveSoftmaxLossAndGradient(centerVec, 1, vec, dataset)
+        return loss, gradOutsideVecs
+    gradcheck_naive(temp, dummy_vectors, "naiveSoftmaxLossAndGradient gradOutsideVecs")
+
+  loss, gradCenterVec, gradOutsideVecs = naiveSoftmaxLossAndGradient(np.array([5, 6]), 1, np.array([[1,2],[3,4]]), None)
+  print (loss, gradCenterVec, gradOutsideVecs)
+
